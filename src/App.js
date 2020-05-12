42fd9c01a6b67976ac77afaca6df4b2ff123470c
mateuszJS <mate.walendzik@gmail.com>
2020-05-12 13:23:56 +0200

    test header customization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      Fleek desktop app
+      <div className="app-title-bar">totally custom header and it's also custom dragable area</div>
+      <div className="app-content">
+        Fleek desktop app
+      </div>
     </div>
   );
 }
