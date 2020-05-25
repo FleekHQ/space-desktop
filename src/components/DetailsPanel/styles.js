f55536f1f37f6fdb3172c26a393f1b5c47ebf783
mateuszJS <mate.walendzik@gmail.com>
2020-05-25 14:44:57 +0200

    add details panel

---
+++ src/components/DetailsPanel/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    height: '100%',
+    padding: '13px 20px',
+  },
+  title: {
+    marginBottom: 10,
+  },
+}));
