bbafcf283808c7e11dcff9fd04037321cb93046c
mateuszJS <mate.walendzik@gmail.com>
2020-05-26 11:33:07 +0200

    remove background color

--- src/components/StorageMainView/styles.js
+++ src/components/StorageMainView/styles.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   root: {
     padding: '6px 18px',
-    backgroundColor: theme.palette.palette.gray5,
   },
   header: {
     display: 'flex',
