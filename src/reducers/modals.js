75b5e8eba710ebbc49db88acf4360056a0c85cc5
mateuszJS <mate.walendzik@gmail.com>
2020-06-17 11:12:57 +0200

    wrap cases/default with block in modals reducer

--- src/reducers/modals.js
+++ src/reducers/modals.js
@@ -7,16 +7,17 @@ const defaultState = [];
 
 export default (state = defaultState, action) => {
   switch (action.type) {
-    case OPEN_MODAL:
+    case OPEN_MODAL: {
       return [
         ...state,
         action.payload,
       ];
-
-    case CLOSE_MODAL:
+    }
+    case CLOSE_MODAL: {
       return state.filter((modal) => modal.id !== action.payload);
-
-    default:
+    }
+    default: {
       return state;
+    }
   }
 };
