06a432ecfc2e97a7acd6f1d92e4a50354fba4746
Guillermo Puente <guillermo.ps09@gmail.com>
2020-11-24 18:27:11 -0300

    added events to download and remove fuse installer pkg

--- public/electron/events/fuse.js
+++ public/electron/events/fuse.js
@@ -1,16 +1,31 @@
+const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 const { ipcMain } = require('electron');
 const isDev = require('electron-is-dev');
 const { exec } = require('child_process');
+const downloadFile = require('../utils/downloader');
 
 const EVENT_PREFIX = 'fuse';
+
+const DOWNLOAD_FUSE_EVENT = `${EVENT_PREFIX}:download`;
+const DOWNLOAD_FUSE_ERROR_EVENT = `${DOWNLOAD_FUSE_EVENT}:error`;
+const DOWNLOAD_FUSE_SUCCESS_EVENT = `${DOWNLOAD_FUSE_EVENT}:success`;
+const UPDATE_PROGRESS_DOWNLOAD_FUSE_EVENT = `${DOWNLOAD_FUSE_EVENT}:update`;
+
 const INSTALL_FUSE_EVENT = `${EVENT_PREFIX}:install`;
 const INSTALL_FUSE_ERROR_EVENT = `${INSTALL_FUSE_EVENT}:error`;
 const INSTALL_FUSE_SUCCESS_EVENT = `${INSTALL_FUSE_EVENT}:success`;
 
+const DELETE_FUSE_INSTALLER = `${EVENT_PREFIX}:delete`;
+const DELETE_FUSE_INSTALLER_ERROR = `${DELETE_FUSE_INSTALLER}:error`;
+const DELETE_FUSE_INSTALLER_SUCCESS = `${DELETE_FUSE_INSTALLER}:success`;
+
+const fusePkgUrl = 'https://space-fuse-asset.s3-us-west-2.amazonaws.com/FUSE+for+macOS+3.11.0.pkg';
+const resourceName = 'FuseInstaller.pkg';
+
 /* eslint-disable no-console */
-const registerFuseEvents = () => {
+const registerFuseEvents = (mainWindow) => {
   ipcMain.on(INSTALL_FUSE_EVENT, async (event) => {
     const installFuse = () => new Promise((resolve, reject) => {
       const installerPath = isDev
@@ -63,6 +78,55 @@ const registerFuseEvents = () => {
       console.error(chalk.red(error));
       event.sender.send(INSTALL_FUSE_ERROR_EVENT);
     }
+  });
+
+  ipcMain.on(DOWNLOAD_FUSE_EVENT, async () => {
+    try {
+      const cwd = process.cwd();
+      const [data, headers, writer] = await downloadFile(fusePkgUrl, cwd, resourceName);
+
+      const totalLength = headers['content-length'];
+
+      data.on('data', (chunk) => {
+        mainWindow.webContents.send(UPDATE_PROGRESS_DOWNLOAD_FUSE_EVENT, {
+          totalLength,
+          chunkLength: chunk.length,
+        });
+      });
+
+      data.on('error', (error) => {
+        data.destroy();
+        writer.destroy();
+        console.error('DOWNLOAD_FUSE_ERROR_EVENT', error);
+        mainWindow.webContents.send(DOWNLOAD_FUSE_ERROR_EVENT, error);
+      });
+
+      writer.on('error', (error) => {
+        writer.destroy();
+        console.error('DOWNLOAD_FUSE_ERROR_EVENT', error);
+        mainWindow.webContents.send(DOWNLOAD_FUSE_ERROR_EVENT, error);
+      });
+
+      writer.on('finish', async () => {
+        mainWindow.webContents.send(DOWNLOAD_FUSE_SUCCESS_EVENT);
+      });
+
+      data.pipe(writer);
+    } catch (error) {
+      console.error('DOWNLOAD_FUSE_ERROR_EVENT', error);
+      mainWindow.webContents.send(DOWNLOAD_FUSE_ERROR_EVENT, error);
+    }
+  });
+
+  ipcMain.on(DELETE_FUSE_INSTALLER, (event) => {
+    fs.unlink(path.join(process.cwd(), resourceName), (err) => {
+      if (err) {
+        event.sender.send(DELETE_FUSE_INSTALLER_ERROR, err);
+        return;
+      }
+
+      event.sender.send(DELETE_FUSE_INSTALLER_SUCCESS);
+    });
   });
 };
 
--- src/events/fuse.js
+++ src/events/fuse.js
@@ -1,11 +1,35 @@
 import { ipcRenderer } from 'electron';
 
 const EVENT_PREFIX = 'fuse';
+
+const DOWNLOAD_FUSE_EVENT = `${EVENT_PREFIX}:download`;
+const DOWNLOAD_FUSE_ERROR_EVENT = `${DOWNLOAD_FUSE_EVENT}:error`;
+const DOWNLOAD_FUSE_SUCCESS_EVENT = `${DOWNLOAD_FUSE_EVENT}:success`;
+const UPDATE_PROGRESS_DOWNLOAD_FUSE_EVENT = `${DOWNLOAD_FUSE_EVENT}:update`;
+
 const INSTALL_FUSE_EVENT = `${EVENT_PREFIX}:install`;
 const INSTALL_FUSE_ERROR_EVENT = `${INSTALL_FUSE_EVENT}:error`;
 const INSTALL_FUSE_SUCCESS_EVENT = `${INSTALL_FUSE_EVENT}:success`;
 
-/* eslint-disable import/prefer-default-export */
+const DELETE_FUSE_INSTALLER = `${EVENT_PREFIX}:delete`;
+const DELETE_FUSE_INSTALLER_ERROR = `${DELETE_FUSE_INSTALLER}:error`;
+const DELETE_FUSE_INSTALLER_SUCCESS = `${DELETE_FUSE_INSTALLER}:success`;
+
+/* eslint-disable no-console */
+const registerFuseEvents = () => {
+  ipcRenderer.on(DOWNLOAD_FUSE_ERROR_EVENT, (event, error) => {
+    console.error('DOWNLOAD_FUSE_ERROR_EVENT', error);
+  });
+
+  ipcRenderer.on(DOWNLOAD_FUSE_SUCCESS_EVENT, () => {
+    console.log('DOWNLOAD_FUSE_SUCCESS_EVENT');
+  });
+
+  ipcRenderer.on(UPDATE_PROGRESS_DOWNLOAD_FUSE_EVENT, (event, payload) => {
+    console.log('UPDATE_PROGRESS_DOWNLOAD_FUSE_EVENT', payload);
+  });
+};
+
 export const installFuse = () => new Promise((resolve, reject) => {
   ipcRenderer.send(INSTALL_FUSE_EVENT);
 
@@ -17,3 +41,19 @@ export const installFuse = () => new Promise((resolve, reject) => {
     resolve(result);
   });
 });
+
+export const deleteFuseInstaller = () => new Promise((resolve, reject) => {
+  ipcRenderer.send(DELETE_FUSE_INSTALLER);
+
+  ipcRenderer.on(DELETE_FUSE_INSTALLER_ERROR, (event, err) => {
+    reject(err);
+  });
+
+  ipcRenderer.on(DELETE_FUSE_INSTALLER_SUCCESS, () => {
+    resolve();
+  });
+});
+
+export const downloadFuse = () => ipcRenderer.send(DOWNLOAD_FUSE_EVENT);
+
+export default registerFuseEvents;
